9f9aef1c2638403e364522fecea0395f4fb55381
komronium <komronium@gmail.com>
2025-01-31 21:12:05 +0500

    remove logger

--- handlers/video.py
+++ handlers/video.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 from pathlib import Path
 from aiogram import Router, F
 from aiogram.types import Message, FSInputFile
@@ -6,6 +7,7 @@ from sqlalchemy.orm import Session
 
 from services.converter import convert_video_to_audio
 from services.user_service import UserService
+
 
 router = Router()
 
@@ -20,14 +22,17 @@ async def video_handler(message: Message, db: Session):
 
     await message.bot.send_chat_action(message.chat.id, 'typing')
     processing_msg = await message.reply("Downloading ...")
+
     file = await message.bot.get_file(video.file_id)
     video_path = file.file_path
     audio_path = None
 
     try:
         file_name = Path(video.file_name or video.file_unique_id).stem.lower()
+
         await message.bot.send_chat_action(message.chat.id, 'typing')
         await processing_msg.edit_text("Converting ...")
+
         audio_path = convert_video_to_audio(video_path, f'audios/{file_name}')
         audio_file = FSInputFile(path=audio_path)
         bot = await message.bot.get_me()
