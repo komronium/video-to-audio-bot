d9fa7389ff2a7f8e89318fac74b28036cadc09d1
komronium <komronium@gmail.com>
2025-01-31 21:15:25 +0500

    remove unexpected attr

--- services/converter.py
+++ services/converter.py
@@ -5,7 +5,7 @@ def convert_video_to_audio(video_path: str, output_path: str) -> str:
     try:
         clip = VideoFileClip(video_path)
         audio_path = f'{output_path}.mp3'
-        clip.audio.write_audiofile(audio_path, verbose=False, logger=None)
+        clip.audio.write_audiofile(audio_path, logger=None)
         clip.close()
         return audio_path
     except Exception as e:
