9b7a9155fbc26730ce0b4efc56c86324b2c82091
komronium <komronium@gmail.com>
2025-02-12 01:33:16 +0500

    fix converter error

--- services/converter.py
+++ services/converter.py
@@ -4,7 +4,7 @@ import ffmpeg
 def convert_video_to_audio(video_path: str, output_path: str) -> str:
     try:
         audio_path = f'{output_path}.mp3'
-        ffmpeg. output(audio_path, format='mp3').run(overwrite_output=True, quiet=True)
+        ffmpeg.input(video_path).output(audio_path, format='mp3').run(overwrite_output=True, quiet=True)
         return audio_path
     except Exception as e:
         raise RuntimeError(f'Error during conversion: {e}')
