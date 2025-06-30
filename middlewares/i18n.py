ae2e3c4e64c21a77e3ac3bf46166360316bf040c
komronium <komronium@gmail.com>
2025-06-30 11:53:49 +0500

    i18n

--- middlewares/i18n.py
+++ middlewares/i18n.py
@@ -3,6 +3,14 @@ from aiogram.types import Message
 
 
 class I18nMiddleware(BaseMiddleware):
+    async def __call__(
+            self,
+            handler,
+            event,
+            data
+    ):
+        return await handler(event, data)
+
     async def on_pre_process_message(self, message: Message, data: dict):
         # Foydalanuvchi tilini aniqlash (masalan, Telegram til kodi)
         lang = message.from_user.language_code or 'en'
