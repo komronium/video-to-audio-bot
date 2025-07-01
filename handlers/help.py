07272fc892a8c4c448719c798b071520e1f5ed07
komronium <komronium@gmail.com>
2025-07-02 00:01:48 +0500

    i18n

--- handlers/help.py
+++ handlers/help.py
@@ -1,7 +1,7 @@
 from aiogram import types, Router, F
-from aiogram.filters import Command
-from aiogram.exceptions import TelegramAPIError
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from services.user_service import UserService
 from utils.i18n import i18n
 
 
@@ -12,5 +12,5 @@ router = Router()
     i18n.get_text('help-button', lang) for lang in i18n.LANGUAGES
 ]))
 async def command_help(message: types.Message, db: AsyncSession):
-    lang = await UserService(db).get_lang(message.from_user.id
+    lang = await UserService(db).get_lang(message.from_user.id)
     await message.answer(i18n.get_text('help-text', lang))
