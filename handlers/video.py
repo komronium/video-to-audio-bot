8d902bd8d3ad080b13a1cbec6f434680c8ae642e
Komron <komronium@gmail.com>
2026-01-08 10:09:19 +0500

    ..

--- handlers/video.py
+++ handlers/video.py
@@ -176,7 +176,9 @@ async def process_video(message: Message, db: AsyncSession, video, lang: str):
         audio_file = FSInputFile(path=audio_path)
 
         bot = await message.bot.get_me()
-        await UserService(db).add_conversation(message.from_user.id)
+        await UserService(db).add_conversation(
+            message.from_user.id, is_premium=is_lifetime
+        )
         await processing_msg.delete()
         await message.reply_document(
             audio_file, caption=i18n.get_text("converted-by", lang).format(bot.username)
